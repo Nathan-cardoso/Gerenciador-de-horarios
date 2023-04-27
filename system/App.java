dc36444f82992ea6883a47cec14623fc5a07b2be
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-04-26 22:40:41 -0300

    Add: Escopo inicial do projeto

---
+++ system/App.java
@@ -0,0 +1,7 @@
+package Project.system;
+
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ system/Turma.java
@@ -0,0 +1,5 @@
+package Project.system;
+
+public class Turma {
+    
+}

5d969675339a6bc8341f00692c9e4917bd0c5269
Nathan-cardoso <nathancar.linhares77@gmail.com>
2023-04-28 15:37:23 -0300

    Add: os atributos do componente curricular

--- system/ComponenteCurricular.java
+++ system/ComponenteCurricular.java
@@ -1,5 +1,23 @@
 package Project.system;
 
+import java.util.ArrayList;
+
 public class ComponenteCurricular {
+    private String codDisciplina;
+    private String nomeDisciplina;
+    private int cargaHoraria;
+    private ArrayList<Turma> listTurma;
+
+    public static void addComponenteCurricular(){
+
+    }
     
+
+
+
+
+
+
+    
+
 }
--- system/Turma.java
+++ system/Turma.java
@@ -1,5 +1,5 @@
 package Project.system;
 
 public class Turma {
-    
+      
 }

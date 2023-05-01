da0d658f6a75f0651fd46519d74dd66c9a99bfea
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-04-30 22:19:18 -0300

    Add: primeira definição dos atributos e construtor

--- system/Turma.java
+++ system/Turma.java
@@ -1,5 +1,34 @@
-package Project.system;
+import java.util.ArrayList;
 
 public class Turma {
-      
+    private String turno;
+    private ArrayList<String> matriculaProf = new ArrayList<>(); //Vai ter alterações futuras!
+    private ArrayList<String> codComponenteCurricular = new ArrayList<>();
+    private int horario;
+
+
+    public Turma(String turno,String matriculaProf, String codComponenteCurricular,
+            int horario) {
+            this.turno = turno;
+            this.matriculaProf.add(matriculaProf);
+            this.codComponenteCurricular.add(codComponenteCurricular);
+            this.horario = horario;
+    }
+
+
+
+    public String toString() {
+        return "Turma [turno=" + turno + ", matriculaProf=" + matriculaProf + ", codComponenteCurricular="
+                + codComponenteCurricular + ", horario=" + horario + "]";
+    }
+
+    
+
+    
+
+    
+    
+
+
+
 }

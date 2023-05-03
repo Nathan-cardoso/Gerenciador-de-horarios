08b818e04919d427ef2ab6e944b4da777af865fc
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-03 11:49:34 -0300

    Add get carga horaria do componente

--- system/ComponenteCurricular.java
+++ system/ComponenteCurricular.java
@@ -2,40 +2,41 @@
 public class ComponenteCurricular {
     private String codDisciplina;
     private String nomeDisciplina;
-    private int cargaHoraria;
+    private int cargaHorariaComp;
     private boolean componenteObrigatorio;
     private String semestre;
 
 
     //Construtor
-    public ComponenteCurricular(String codDisciplina, String nomeDisciplina, int cargaHoraria,
+    public ComponenteCurricular(String codDisciplina, String nomeDisciplina, int cargaHorariaComp,
             boolean componenteObrigatorio, String semestre) {
         this.codDisciplina = codDisciplina;
         this.nomeDisciplina = nomeDisciplina;
-        this.cargaHoraria = cargaHoraria;
+        this.cargaHorariaComp = cargaHorariaComp;
         this.componenteObrigatorio = componenteObrigatorio;
         this.semestre = semestre;
     }
-<<<<<<< HEAD
-
-    public int getCargaHoraria() {
-        return 0;
+ 
+    public int getCargaHorariaComp() {
+        return cargaHorariaComp;
     }
-    
 
 
 
 
-=======
->>>>>>> 3dc0a505ebdef3a85b1250af532ef27f07c67da9
-
-
-    
     public String toString() {
         return "ComponenteCurricular [codDisciplina=" + codDisciplina + ", nomeDisciplina=" + nomeDisciplina
-                + ", cargaHoraria=" + cargaHoraria + ", componenteObrigatorio=" + componenteObrigatorio + ", semestre="
+                + ", cargaHorariaComp=" + cargaHorariaComp + ", componenteObrigatorio=" + componenteObrigatorio + ", semestre="
                 + semestre + "]";
     }
+
+
+
+
+
+
+
+
 
 
 }

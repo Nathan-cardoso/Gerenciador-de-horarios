0197992d468c9ff4ae2a40271a3df11b7bfdb209
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-03 22:36:43 -0300

    Add: classe de conexão

--- system/ElephantSQLConnection.java
+++ system/ElephantSQLConnection.java
@@ -1,21 +1,22 @@
-
+/*Essa classe foi criada para automatizar o processo de conexão e desconexão com o banco de dados, tendo em vista
+ * que esse processo requer algumas linhas repedidas de código.
+ */
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ElephantSQLConnection {
-
+public class ElephantSQLConnection { //O nome do serviço que está o banco é elephant sql site:https://www.elephantsql.com/
+    //Variaveis para a conexão.
     private static final String URL = "jdbc:postgresql://tuffi.db.elephantsql.com:5432/onhjfeum";
     private static final String USER = "onhjfeum";
     private static final String PASSWORD = "ZmAXUbTMUwf4Mf3Ia8WD31SZ6_7d9sq_";
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() throws SQLException { //método para conexão
         Connection connection = null;
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Conexão com banco de dados estabelecida com sucesso.");
         } catch (ClassNotFoundException e) {
             System.out.println("Driver PostgreSQL não encontrado.");
         } catch (SQLException e) {
@@ -24,11 +25,10 @@ public class ElephantSQLConnection {
         return connection;
     }
 
-    public static void closeConnection(Connection connection) {
+    public static void closeConnection(Connection connection) {//Método que desconecta 
         try {
             if (connection != null) {
                 connection.close();
-                System.out.println("Conexão com banco de dados fechada.");
             }
         } catch (SQLException e) {
             System.out.println("Erro ao fechar a conexão com o banco de dados: " + e.getMessage());

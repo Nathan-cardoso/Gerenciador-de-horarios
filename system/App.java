423705a5fe9f4070694a564598dcc352d99b5cb2
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-07 15:13:56 -0300

    Criando uma classe para ter a impressão dos menus

--- system/App.java
+++ system/App.java
@@ -8,14 +8,14 @@ public class App {
         ArrayList<ComponenteCurricular> componenteCurriculara= new ArrayList<>();
 
         do{
-            menuPrincipal();
+            Menu.menuPrincipal();
             System.out.print("Digite: ");
             opcao = sc.nextShort();
 
             switch(opcao){
                 case 1:
                 do{
-                    menuProf();
+                    Menu.menuProf();
                     System.out.print("Digite: ");
                     opcao = sc.nextShort();
         
@@ -84,7 +84,7 @@ public class App {
 
                 case 2:
                 do{
-                    menuCompCurricular();
+                    Menu.menuCompCurricular();
                     System.out.print("Digite: ");
                     opcao = sc.nextShort();
         
@@ -166,7 +166,7 @@ public class App {
 
                 case 3:
                 do{
-                    menuTurma();
+                    Menu.menuTurma();
                     System.out.print("Digite: ");
                     opcao = sc.nextShort();
         
@@ -186,7 +186,7 @@ public class App {
                         System.out.println("Atribua o componente curricular: ");
                         String cc = sc.nextLine();
             
-                        if(ComponenteCurricular.verificacaoDeCodigo(ComponenteCurricular.buscarCodigoComponente(cc))){
+                        if(verificacaoDeCodigo(ComponenteCurricular.buscarCodigoComponente(cc))){
                             Turma turma = new Turma(codturma, horario, semestre, cc);
                             turma.cadastrarTurma();
                         }else{
@@ -234,73 +234,13 @@ public class App {
         sc.close();
     }
 
-    public static void menuPrincipal(){
-        System.out.println("\n|============ Menu =============|");
-        System.out.println("| [1] - Professor\t\t|");
-        System.out.println("| [2] - Componente Curricular\t|");
-        System.out.println("| [3] - Turma\t\t\t|");
-        System.out.println("| [4] - Sair\t\t\t|");
-        System.out.println("---------------------------------");
-        System.out.println("Para consultar as áreas digite o número correspondente");
-
-    }
-
-
-    public static void menuProf() {
-        /*Cadastrar Professor
-            Editar Professor
-            Ver dados de um Professor
-            Listar Professores
-            Excluir Professor
- */
-        System.out.println("\n|========== Professor ===========|");
-        System.out.println("|[1] - Cadastrar Professor\t |");
-        System.out.println("|[2] - Editar Professor\t\t |");
-        System.out.println("|[3] - Ver dados de um professor |");
-        System.out.println("|[4] - Listar Professor\t\t |");
-        System.out.println("|[5] - Excluir professor\t |");
-        System.out.println("|[6] - Voltar\t\t\t |\n");
-    }
-
-    public static void menuCompCurricular(){
-        /*Cadastrar Componente Curricular
-        Editar Componente Curricular
-        Ver dados de um Componente Curricular
-        Listar Componentes Curriculares
-        Excluir Componente Curricular
- */
-        System.out.println("\n|=========== Componete Curricular ==============|");
-        System.out.println("|[1] - Cadastrar Componente Curricular\t\t|");
-        System.out.println("|[2] - Editar Componente Curricular\t\t|");
-        System.out.println("|[3] - Ver dados de um Componente Curricular\t|");
-        System.out.println("|[4] - Listar Componente Curricular\t\t|");
-        System.out.println("|[5] - Excluir Componente Curricular\t\t|");
-        System.out.println("|[6] - Voltar\t\t\t\t\t|\n");
-    }
-
-    public static void menuTurma(){
-        /*Cadastrar Turma
-        Editar Turma
-        Ver dados de uma Turma
-        Listar todas as Turmas (em formato de tabela)
-        Listar Turmas por semestre (em formato de tabela)
-        Listar Turmas por Professor (em formato de tabela)
-        Excluir Turma
- */
-        System.out.println("\n|=========== Turma =============|");
-        System.out.println("|[1] - Cadastrar Turma\t\t|");
-        System.out.println("|[2] - Editar Turma\t\t|");
-        System.out.println("|[3] - Ver dados de uma Turma\t|");
-        System.out.println("|[4] - Listar Turma\t\t|");
-        System.out.println("|[5] - Excluir Turma\t\t|");
-        System.out.println("|[6] - Voltar\t\t\t|\n");
-
-    }
-
-    public static void subMenuTurma(){
-        /*Listar todas as Turmas (em formato de tabela)
-        Listar Turmas por semestre (em formato de tabela)
-        Listar Turmas por Professor (em formato de tabela) */
+    //Método para ajudar no tratamento de alguns erros
+    public static boolean verificacaoDeCodigo(String cod){
+        if(cod == null){
+            return false;
+        }else{
+            return true;
+        }
     }
 
 
---
+++ system/Menu.java
@@ -0,0 +1,65 @@
+public class Menu {
+    
+    public static void menuPrincipal(){
+        System.out.println("\n|============ Menu =============|");
+        System.out.println("| [1] - Professor\t\t|");
+        System.out.println("| [2] - Componente Curricular\t|");
+        System.out.println("| [3] - Turma\t\t\t|");
+        System.out.println("| [4] - Sair\t\t\t|");
+        System.out.println("---------------------------------");
+        System.out.println("Para consultar as áreas digite o número correspondente");
+
+    }
+
+
+    public static void menuProf() {
+        /*Cadastrar Professor
+            Editar Professor
+            Ver dados de um Professor
+            Listar Professores
+            Excluir Professor
+ */
+        System.out.println("\n|========== Professor ===========|");
+        System.out.println("|[1] - Cadastrar Professor\t |");
+        System.out.println("|[2] - Editar Professor\t\t |");
+        System.out.println("|[3] - Ver dados de um professor |");
+        System.out.println("|[4] - Listar Professor\t\t |");
+        System.out.println("|[5] - Excluir professor\t |");
+        System.out.println("|[6] - Voltar\t\t\t |\n");
+    }
+
+    public static void menuCompCurricular(){
+        /*Cadastrar Componente Curricular
+        Editar Componente Curricular
+        Ver dados de um Componente Curricular
+        Listar Componentes Curriculares
+        Excluir Componente Curricular
+ */
+        System.out.println("\n|=========== Componete Curricular ==============|");
+        System.out.println("|[1] - Cadastrar Componente Curricular\t\t|");
+        System.out.println("|[2] - Editar Componente Curricular\t\t|");
+        System.out.println("|[3] - Ver dados de um Componente Curricular\t|");
+        System.out.println("|[4] - Listar Componente Curricular\t\t|");
+        System.out.println("|[5] - Excluir Componente Curricular\t\t|");
+        System.out.println("|[6] - Voltar\t\t\t\t\t|\n");
+    }
+
+    public static void menuTurma(){
+        /*Cadastrar Turma
+        Editar Turma
+        Ver dados de uma Turma
+        Listar todas as Turmas (em formato de tabela)
+        Listar Turmas por semestre (em formato de tabela)
+        Listar Turmas por Professor (em formato de tabela)
+        Excluir Turma
+ */
+        System.out.println("\n|=========== Turma =============|");
+        System.out.println("|[1] - Cadastrar Turma\t\t|");
+        System.out.println("|[2] - Editar Turma\t\t|");
+        System.out.println("|[3] - Ver dados de uma Turma\t|");
+        System.out.println("|[4] - Listar Turma\t\t|");
+        System.out.println("|[5] - Excluir Turma\t\t|");
+        System.out.println("|[6] - Voltar\t\t\t|\n");
+
+    }
+}

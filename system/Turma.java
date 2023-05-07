829e7cc87271dba0c45a6d0d09d14a1b29869b5b
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-07 17:36:05 -0300

    turma upload

--- system/Turma.java
+++ system/Turma.java
@@ -1,7 +1,7 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
+
 /*
 Uma turma pode ter vários professores
 Pode haver mais de uma turma do mesmo componente curricular
@@ -10,25 +10,18 @@ Turmas ministradas por um mesmo professor não podem ter o mesmo horário
  */
 public class Turma {
     private String codTurma;
-    private ArrayList<String> prof = new ArrayList<>();
+    private String codProf;
     private String codComponente;
     private String horario;
     private int semestre;
 
-    public Turma(String codTurma, String horario, int semestre,String codComponente) {
+    public Turma(String codTurma, String horario, int semestre,String codComponente, String codProf) {
         this.codTurma = codTurma;
         this.horario = horario;
         this.semestre = semestre;
         this.codComponente = codComponente;
+        this.codProf = codProf;
     }
-
-    
-
-    // public void addProfessor(String codProf){ //Melhorias futuras
-    //     prof.add(codProf);
-    // }
-
-
 
     public void setCodComponente(String codComponente) {
         this.codComponente = codComponente;
@@ -41,23 +34,27 @@ public class Turma {
 
         try {
             Connection connection = ElephantSQLConnection.getConnection();
-            String query = "INSERT INTO turma (cod_turma, horario, semestre,cod_componente) values (?,?,?,?)";
+            String query = "INSERT INTO turma (cod_turma, horario, semestre,cod_componente) values (?,?,?,?,?)";
             pstm = connection.prepareStatement(query);
 
             pstm.setString(1, this.codTurma);
             pstm.setString(2, this.horario);
             pstm.setInt(3,this.semestre);
             pstm.setString(4, this.codComponente);
+            pstm.setString(5, this.codProf);
             int linhasVerifica = pstm.executeUpdate();
             if(linhasVerifica > 0){
-                System.out.println("Dados iniciais cadastrados");
+                System.out.println("Turma cadastrada...");
             }else{
                 System.out.println("Erro no cadastro...");
             }
 
+            pstm.close();
+            connection.close();
+
             
         } catch (SQLException e) {
-           System.out.println("Erro: " + e.getMessage());
+           System.out.println("Já tem uma turma cadastrada com esse codigo... " + e.getMessage());
         }
 
 

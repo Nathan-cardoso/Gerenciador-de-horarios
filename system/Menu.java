8e27a0788b476d0a6a72c984eefee8c6465abaf3
Sr-Cardoso <nathancar.linhares77@gmail.com>
2023-05-08 11:45:29 -0300

    Add menu dos horarios

--- system/Menu.java
+++ system/Menu.java
@@ -62,4 +62,31 @@ public class Menu {
         System.out.println("|[6] - Voltar\t\t\t|\n");
 
     }
+
+    public static void exibirHorariosManha(){
+        System.out.println("1 - 07h00 - 07h55");
+        System.out.println("2 - 07h55 - 08h50");
+        System.out.println("3 - 08h50 - 09h45");
+        System.out.println("4 - 09h55 - 10h50");
+        System.out.println("5 - 10h50 - 11h45");
+        System.out.println("6 - 11h45 - 12h40");
+    }
+
+    public static void exibirHorariosTarde(){
+        System.out.println("1 - 13h00 - 13h55");
+        System.out.println("2 - 13h55 - 14h50");
+        System.out.println("3 - 14h50 - 15h45");
+        System.out.println("4 - 15h45 - 16h50");
+        System.out.println("5 - 16h50 - 17h45");
+        System.out.println("6 - 17h45 - 18h40");
+    }
+
+    public static void exibirHorariosNoite(){
+        System.out.println("1 - 18h50 - 19h45");
+        System.out.println("2 - 19h45 - 20h40");
+        System.out.println("3 - 20h50 - 21h35");
+        System.out.println("4 - 21h35 - 22h30");
+    }
+
+
 }
